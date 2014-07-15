a57d4e710cf2d81818707ae65188e5febc32c1f9
Dmitriy Vasilev <argon@argon-VirtualBox.(none)>
2014-07-15 14:18:34 +0700

    simplest deletion

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var myApp = angular.module('myApp', ['infinite-scroll']);
+
 myApp.controller('DemoController', function($scope, $timeout, $rootElement) {
     $scope.$rootElement = $rootElement;
     $scope.$timeout= $timeout;
@@ -13,10 +14,28 @@ myApp.controller('DemoController', function($scope, $timeout, $rootElement) {
         {id: 8},
     ];
 
+    $scope.hide_topmost_items = function(){
+        _.each($scope.posts, function(post){
+            var elem = $('#post_' + post.id);
+            if (elem.position().top + elem.height() + 500 < window.pageYOffset){
+                elem.hidden = true;
+                window.scrollTo(0, window.pageYOffset - elem.height());
+                elem.hide();
+                console.log('#post_' + post.id + ' is hidden now')
+            }
+        });
+        //window.pageYOffset
+        //('div.post:eq(2)').position()
+        //e.height()
+    };
+
+    $(window).on('scroll', $scope.hide_topmost_items);
+
     $scope.loadMore = function() {
-        var last = $scope.posts[$scope.posts.length - 1];
+        var length = $scope.posts.length;
+        var last = $scope.posts[length - 1];
         for(var i = 1; i <= 8; i++) {
-            $scope.posts.push({id: i});
+            $scope.posts.push({id: i + length});
         }
     };
 });
