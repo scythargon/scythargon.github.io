25404499a7b229969c39718d805419bf7dd202b0
Dmitriy Vasilev <argon@argon-VirtualBox.(none)>
2014-07-15 13:13:32 +0700

    indentation fix

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 var myApp = angular.module('myApp', ['infinite-scroll']);
 myApp.controller('DemoController', function($scope, $timeout, $rootElement) {
-  $scope.$rootElement = $rootElement;
-  $scope.$timeout= $timeout;
-  $scope.posts = [
-      {id: 1},
-      {id: 2},
-      {id: 3},
-      {id: 4},
-      {id: 5},
-      {id: 6},
-      {id: 7},
-      {id: 8},
-  ];
+    $scope.$rootElement = $rootElement;
+    $scope.$timeout= $timeout;
+    $scope.posts = [
+        {id: 1},
+        {id: 2},
+        {id: 3},
+        {id: 4},
+        {id: 5},
+        {id: 6},
+        {id: 7},
+        {id: 8},
+    ];
 
-  $scope.loadMore = function() {
-    var last = $scope.posts[$scope.posts.length - 1];
-    for(var i = 1; i <= 8; i++) {
-      $scope.posts.push({id: i});
-    }
-  };
+    $scope.loadMore = function() {
+        var last = $scope.posts[$scope.posts.length - 1];
+        for(var i = 1; i <= 8; i++) {
+            $scope.posts.push({id: i});
+        }
+    };
 });
-
