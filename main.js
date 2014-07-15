3cc3118dd8e94b2f7a949e6b6cd8711411a55776
Dmitriy Vasilev <argon@argon-VirtualBox.(none)>
2014-07-15 13:09:04 +0700

    now with objects

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ var myApp = angular.module('myApp', ['infinite-scroll']);
 myApp.controller('DemoController', function($scope, $timeout, $rootElement) {
   $scope.$rootElement = $rootElement;
   $scope.$timeout= $timeout;
-  $scope.images = [1, 2, 3, 4, 5, 6, 7, 8];
+  $scope.posts = [
+      {id: 1},
+      {id: 2},
+      {id: 3},
+      {id: 4},
+      {id: 5},
+      {id: 6},
+      {id: 7},
+      {id: 8},
+  ];
 
   $scope.loadMore = function() {
-    var last = $scope.images[$scope.images.length - 1];
+    var last = $scope.posts[$scope.posts.length - 1];
     for(var i = 1; i <= 8; i++) {
-      $scope.images.push(last + i);
+      $scope.posts.push({id: i});
     }
-    $scope.$timeout(function(){
-       $scope.$rootElement.find('[infinite-scroll]').trigger('scroll-check');
-    });
   };
 });
 
